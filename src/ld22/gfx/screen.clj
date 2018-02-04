d7b3b6983158a8f5cc21eaaf2b3fdd7ca21b6bad
David Ongaro <david.ongaro@hamburg.de>
2018-02-05 02:28:54 +0800

    A few small optimizations

--- src/ld22/gfx/screen.clj
+++ src/ld22/gfx/screen.clj
@@ -1,4 +1,7 @@
-(ns ld22.gfx.screen)
+(ns ld22.gfx.screen
+  (:require [ld22.level.macros :refer [<< >>]]))
+
+(set! *unchecked-math* true)
 
 (defrecord Screen
     [^int x-offset
@@ -17,28 +20,32 @@
 (defn render
   "Render tile from sprite sheet based on screen coordinates,
   tile number, colors and mirror options"
-  [screen xp yp tile colors & options]
+  [^Screen screen xp yp tile colors & options]
   (let [sheet (:sheet screen)
         screen-pixels ^ints (:pixels screen)
         sheet-pixels ^ints (sheet :pixels)
-        sheet-width ^int (sheet :width)
+;        sheet-width ^int (sheet :width)
         x-offs (:x-offset screen)
         y-offs (:y-offset screen)
         xp (- ^int xp (int x-offs))
         yp (- ^int yp (int y-offs))
         x-tile (bit-and ^int tile 31)
-        y-tile (bit-shift-right ^int tile 5)
-
-        ;; t-offs = x-tile * 8 + y-tile * 8 * sheet.width
-        t-offs (+ (bit-shift-left x-tile 3)
-                  (* (bit-shift-left y-tile 3) ^int (sheet :width)))
+        y-tile (>> ^int tile 5)
+        t-offs (+ (<< x-tile 3)
+                  (<< y-tile 11)) ; 8 bit for sheet width, 3 for tile width
         {:keys [mirror-x mirror-y]} (apply hash-map options)
-        xss (if mirror-x 7 0)
+        xss (int (if mirror-x 7 0))
         xd (if mirror-x dec inc)
-        yss (if mirror-y 7 0)
+        yss ^int (if mirror-y 7 0)
         yd (if mirror-y dec inc)
         h (:h screen)
         w (:w screen)
+        colors (int-array (list
+                           (bit-and colors 0xff)
+                           (bit-and (>> colors 8) 0xff)
+                           (bit-and (>> colors 16) 0xff)
+                           (bit-and (>> colors 24) 0xff))
+                           )
         ]
     (loop [y 0
            ys yss
@@ -46,27 +53,26 @@
            xs xss
            ]
       (cond
+        ;; All rows done?
         (or (= y 8) (>= (+ y yp) ^int h)) true
 
+        ;; Next row?
         (or (= x 8)
             (neg? (+ y yp))
             (>= (+ x xp) ^int w))
         (recur (inc y) (yd ys) 0 xss)
 
+        ;; left crop
         (neg? (+ x xp))
         (recur y ys (inc x) (xd xs))
         :else
 
         ;; Map the color encoded in the first 2 bits to an index
         ;; into the 255 color palette by the given 'colors' parameter
-        (let [col (-> ^int colors
-                      (bit-shift-right
-                       (bit-shift-left
-                        (aget sheet-pixels
-                              (+ (int xs) (* (int ys) ^int sheet-width)
-                                 t-offs))
-                        3))
-                      (bit-and 0xff))
+        (let [col
+              (aget colors (aget sheet-pixels
+                            (+ (int xs) (bit-shift-left (int ys) 8)
+                               t-offs)))
               ]
           (if (< col 255)
             (aset screen-pixels
